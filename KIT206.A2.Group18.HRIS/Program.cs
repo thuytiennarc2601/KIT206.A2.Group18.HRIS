5fda0087aa39f5c2f552cdd7046de5257f7bfcf8
thuyt <thuyt@DESKTOP-OUTGB3M>
2022-04-07 13:16:54 +1000

    Add project files.

---
+++ KIT206.A2.Group18.HRIS/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KIT206.A2.Group18.HRIS
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
